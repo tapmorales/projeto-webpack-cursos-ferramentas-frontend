2f959b7fab61407efecc466533b7a9d861842d73
Daniel Tapias Morales <tapmorales@gmail.com>
2020-02-22 10:32:52 +0000

    babel works well

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,17 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/env"],
+            // presets: ["@babel/preset-env"],
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: ["last 2 versions", "ie 11"]
+                  }
+                }
+              ]
+            ],
             plugins: [
               ["@babel/plugin-proposal-class-properties"],
               [
